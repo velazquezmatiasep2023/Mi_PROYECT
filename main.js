589c462b3a3970eaef0738434e5a0fc440ed4e00
MatiasV <velazquezmatiasep2023@gmail.com>
2026-09-22 05:02:24 -0300

    Agrego este java scritp

---
+++ main.js
@@ -0,0 +1,52 @@
+// Ejercicio simple mostrando nombre,edad y fecha de nacimiento
+// const nombre= "Matias";
+// let año=25;
+// const año_de_naciemiento= 2025-año;
+// const ciudad= "Salta";
+// console.log (`Nombre: ${nombre}\n Edad: ${año}\n Ciudad: ${ciudad}\n Año de nacimiento:${año_de_naciemiento}`)
+// Ejercicio con listas
+// productos=["Naranja","Manzana","Banana"];
+// console.log(productos)
+// console.log(`${productos[0]}`)
+// console.log(`${productos[2]}`)
+// productos.push("Mandarina");
+// console.log(`${productos}`)
+// console.log(`${productos.length}`)
+// for (let i= 0;i< productos.length;i++)
+//     {
+//         console.log(`Producto:${productos[i]}`)
+//     }
+// Ejercicio con funcion
+// function calculartotal(a,b)
+// {
+//     let cantidad=a
+//     let precio=b
+//     let total=cantidad*precio
+//     console.log(total)
+//     return total
+// }
+// calculartotal(1000,5)
+// calculartotal(100,4)
+// calculartotal(10,2)
+// Segundo ejercicio con funcion
+// Creá una función llamada buscarProducto que reciba la lista y un producto.
+// La función debe recorrer la lista y decir si el producto existe.
+// let productos = ["Manzana", "Naranja", "Banana", "Tomate"];
+
+// function recorrer(a) {
+
+//     for (let i = 0; i < productos.length; i++) {
+
+//         let producto = productos[i];
+
+//         if (a === producto) {
+//             console.log("El producto existe");
+//         }
+//     }
+// }
+// recorrer("Manzanaa")
+// let numeros=[1,2,3,4,5,6,7,8,9,10]
+// const doble=numeros.map(numeropar=>numeropar*2)
+// console.log(doble)
+// const filtro=numeros.filter(numerosa=>numerosa%3===0)
+// console.log(filtro)
